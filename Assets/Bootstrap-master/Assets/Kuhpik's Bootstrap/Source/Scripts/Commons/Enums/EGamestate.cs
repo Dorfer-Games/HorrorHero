ff3fcdb204b3d0291a762b4f7de8954e2db17f10
Sinekrovka <77225026+Sinekrovka@users.noreply.github.com>
2021-05-25 22:24:00 +0600

    FirstCommit

---
+++ Assets/Bootstrap-master/Assets/Kuhpik's Bootstrap/Source/Scripts/Commons/Enums/EGamestate.cs
@@ -0,0 +1,19 @@
+﻿namespace Kuhpik
+{
+    public enum EGamestate
+    {
+        // Don't change int values in the middle of development.
+        // Otherwise all of your settings in inspector can be messed up.
+
+        Loading = 1,
+        Start = 2,
+        Game = 3,
+        Result = 10
+
+        // Extend just like that
+        //
+        // Shop = 20,
+        // Settings = 30,
+        // Revive = 100
+    }
+}
