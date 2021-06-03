d88dc7d08194ddebcd47307d012cd99caa64b57e
Sinekrovka <77225026+Sinekrovka@users.noreply.github.com>
2021-06-03 19:20:24 +0600

    Edit Prefabs

--- Assets/Source/Scripts/Components/SoundObject.cs
+++ Assets/Source/Scripts/Components/SoundObject.cs
@@ -13,7 +13,7 @@ public class SoundObject : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.transform.tag == "Murder")
+        if (other.transform.tag != "Floor")
         {
             sound.Play();
         }
