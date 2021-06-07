6be549042ff785f22f551b1e3982c96b6650b71c
Sinekrovka <77225026+Sinekrovka@users.noreply.github.com>
2021-06-07 15:39:01 +0600

    Edit Camera

--- Assets/Source/Scripts/GameState/MainCamera.cs
+++ Assets/Source/Scripts/GameState/MainCamera.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Numerics;
 using Kuhpik;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
-public class MainCamera : GameSystem, IUpdating
+public class MainCamera : GameSystem, IIniting, IUpdating
 {
     [SerializeField] private Camera cam;
+
+    private Vector3 rotate;
+
+    void IIniting.OnInit()
+    {
+        rotate = cam.transform.rotation.eulerAngles;
+    }
     // Update is called once per frame
    void IUpdating.OnUpdate()
     {
         Vector3 pos = cam.transform.position;
         pos.z = game.murder.transform.position.z - 7;
-        cam.transform.position = pos;/**/
+        cam.transform.position = pos;
+
+        rotate.y = game.murder.transform.rotation.eulerAngles.y;
+        cam.transform.rotation = Quaternion.Euler(rotate); /**/
         //Vector3 targetPosition = murder.TransformPoint(new Vector3(0, 6, -7));
         //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-        
+
     }
 }
